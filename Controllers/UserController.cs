302c1e4d8a51dd0b10402eee2eb5ee80a6295b35
GABRIEL MENDES FONSECA <124696627+Gmendesfonseca@users.noreply.github.com>
2024-05-08 22:13:21 -0300

    Update UserController.cs

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -13,12 +13,76 @@ namespace ImobSystem_API.Controllers
         /* Create User */
         public static void MapUserEndpoint(this WebApplication app)
         {
+            /* Prefix User Route */
             var groupUser = app.MapGroup("user");
 
-            groupUser.MapPost("/create", async (AddUserRequest requestUser, AppDbContext context) =>
+            /* Create User */
+            groupUser.MapPost("/create", async (AddUserRequest requestAddUser, AppDbContext context) =>
             {
-                var newUser = await context.Users.AnyAsync(user => user.getEmail() = requestUser.email);
-            }
+                var newUser = new User(requestAddUser.name, requestAddUser.email, requestAddUser.password, requestAddUser.age);
+
+                var checkUserExists = await context.Users.AnyAsync(u => u.getEmail() == newUser.getEmail());
+
+                if (checkUserExists)
+                {
+                    return Results.BadRequest("User already exists");
+                }
+
+                await context.Users.AddAsync(newUser);
+                await context.SaveChangesAsync();
+
+                return Results.Redirect($"/user/checkInfo/{newUser.getId()}");
+            });
+
+            /* Check yourself user info */
+            groupUser.MapGet("/checkInfo/{id}", async (uint id, AppDbContext context) =>
+            {
+                var user = await context.Users.FindAsync(id);
+
+                if (user == null)
+                {
+                    return Results.NotFound();
+                }
+
+                return Results.Ok(user);
+            });
+
+
+            /* Update User */
+            groupUser.MapPut("/update/{id}", async (UpdateUserRequest requestUpdateUser, AppDbContext context) =>
+            {
+                var user = await context.Users.FindAsync(requestUpdateUser.id);
+
+                if (user == null)
+                {
+                    return Results.NotFound();
+                }
+
+                user.setName(requestUpdateUser.name);
+                user.setEmail(requestUpdateUser.email);
+                user.setPassword(requestUpdateUser.password);
+                user.setAge(requestUpdateUser.age);
+
+                await context.SaveChangesAsync();
+
+                return Results.Ok(user);
+            });
+
+            /* Delete User */
+            groupUser.MapDelete("/delete/{id}", async (uint id, AppDbContext context) =>
+            {
+                var user = await context.Users.FindAsync(id);
+
+                if (user == null)
+                {
+                    return Results.NotFound("User not found");
+                }
+
+                context.Users.Remove(user);
+                await context.SaveChangesAsync();
+
+                return Results.Ok();
+            });
         }
 
     }
