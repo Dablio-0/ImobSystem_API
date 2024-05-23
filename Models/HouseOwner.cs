3115af3b73c987b045a9827aa906a30b29a8c6e5
Wellington de Elias Rodrigues <wellingtonrod.work@gmail.com>
2024-05-22 23:44:33 -0300

    Add Relation's Model

---
+++ Models/HouseOwner.cs
@@ -0,0 +1,27 @@
+﻿namespace ImobSystem_API.Models
+{
+    public class HouseOwner
+    {
+        public uint HouseId { get; set; }
+        public House House { get; set; }
+
+        public uint OwnerId { get; set; }
+        public Owner Owner { get; set; }
+
+        public HouseOwner()
+        {
+            this.HouseId = this.OwnerId = 0;
+            House = new House();
+            Owner = new Owner();
+        }
+
+        public HouseOwner(uint houseId, uint ownerId)
+        {
+            HouseId = houseId;
+            OwnerId = ownerId;
+            House = new House();
+            Owner = new Owner();
+
+        }
+    }
+}
---
+++ Models/TenantAgreement.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ImobSystem_API.Models
+{
+    public class TenantAgreement
+    {
+        public uint TenantId { get; set; }
+        public Tenant Tenant { get; set; }
+
+        public uint HouseId { get; set; }
+        public House House { get; set; }
+
+        public TenantAgreement()
+        {
+            this.TenantId = this.HouseId = 0;
+            Tenant = new Tenant();
+            House = new House();
+        }
+
+        public TenantAgreement(uint tenantId, uint houseId)
+        {
+            TenantId = tenantId;
+            HouseId = houseId;
+            Tenant = new Tenant();
+            House = new House();
+        }
+    }
+}
